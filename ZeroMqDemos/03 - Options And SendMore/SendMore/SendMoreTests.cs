041005463776606a13ffe78296b37ccdbbc81312
sacha.barber@gmail.com <sacha.barber@gmail.com>
2014-08-26 07:45:48 +0100

    Add 03 post examples

---
+++ ZeroMqDemos/03 - Options And SendMore/SendMore/SendMoreTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NUnit.Framework;
+
+namespace SendMore
+{
+    [TestFixture]
+    public class SendMoreTests
+    {
+        [Test]
+        public void SendMoreTest()
+        {
+            using (NetMQContext ctx = NetMQContext.Create())
+            {
+                using (var server = ctx.CreateResponseSocket())
+                {
+                    server.Bind("tcp://127.0.0.1:5556");
+
+                    using (var client = ctx.CreateRequestSocket())
+                    {
+                        client.Connect("tcp://127.0.0.1:5556");
+
+                        //client send message
+                        client.SendMore("A");
+                        client.Send("Hello");
+
+                        //server receive 1st part
+                        bool more;
+                        string m = server.ReceiveString(out more);
+                        Assert.AreEqual("A", m);
+                        Assert.IsTrue(more);
+
+                        //server receive 2nd part
+                        string m2 = server.ReceiveString(out more);
+                        Assert.AreEqual("Hello", m2);
+                        Assert.False(more);
+
+                        //server send message, this time use NetMqMessage
+                        //which will be sent as frames if the client calls
+                        //ReceieveMessage()
+                        var m3 = new NetMQMessage();
+                        m3.Append("From");
+                        m3.Append("Server");
+                        server.SendMessage(m3);
+
+
+                        //client receive
+                        var m4 = client.ReceiveMessage();
+                        Assert.AreEqual(2, m4.FrameCount);
+                        Assert.AreEqual("From", m4[0].ConvertToString());
+                        Assert.AreEqual("Server", m4[1].ConvertToString());
+
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ZeroMqDemos/03 - Options And SendMore/ZeroMqIdentity/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace ZeroMqIdentity
+{
+    public class Program : IDisposable
+    {
+        private List<RequestSocket> clients = new List<RequestSocket>();
+
+        public void Run()
+        {
+            using (NetMQContext ctx = NetMQContext.Create())
+            {
+                using (var server = ctx.CreateRouterSocket())
+                {
+                    server.Bind("tcp://127.0.0.1:5556");
+
+                    CreateClient(ctx, "A_");
+                    CreateClient(ctx, "B_");
+                    CreateClient(ctx, "C_");
+                    CreateClient(ctx, "D_");
+
+                    while (true)
+                    {
+
+                        var clientMessage = server.ReceiveMessage();
+                        Console.WriteLine("========================");
+                        Console.WriteLine(" INCOMING CLIENT MESSAGE ");
+                        Console.WriteLine("========================");
+                        for (int i = 0; i < clientMessage.FrameCount; i++)
+                        {
+                            Console.WriteLine("Frame[{0}] = {1}", i, 
+                                clientMessage[i].ConvertToString());
+                        }
+
+                        var clientAddress = clientMessage[0];
+                        var clientOriginalMessage = clientMessage[2].ConvertToString();
+                        string response = string.Format("{0} back from server", 
+                            clientOriginalMessage);
+
+                        // "B_" client is special
+                        if (clientOriginalMessage.StartsWith("B_"))
+                        {
+                            response = string.Format(
+                                "special Message for 'B' back from server");
+                        }
+
+                        var messageToClient = new NetMQMessage();
+                        messageToClient.Append(clientAddress);
+                        messageToClient.AppendEmptyFrame();
+                        messageToClient.Append(response);
+                        server.SendMessage(messageToClient);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+
+        private void CreateClient(NetMQContext ctx, string prefix)
+        {
+            Task.Run(() =>
+            {
+                var client = ctx.CreateRequestSocket();
+                clients.Add(client);
+                client.Connect("tcp://127.0.0.1:5556");
+                client.Send(string.Format("{0}Hello", prefix));
+
+                //read client message
+                var echoedServerMessage = client.ReceiveString();
+                Console.WriteLine(
+                    "\r\nClient Prefix is : '{0}', Server Message : '{1}'",
+                    prefix, echoedServerMessage);
+
+            });
+        }
+
+        public void Dispose()
+        {
+            foreach (var client in clients)
+            {
+                client.Dispose();
+            }
+        }
+    }
+}
